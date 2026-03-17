8169a7699613298936de505207ab5b1e6b082db1
Pepito <juniorfotio@teccart.com>
2026-03-17 17:43:55 -0400

    exercice 8 sans bonus termine

--- src/exercise/08.tsx
+++ src/exercise/08.tsx
@@ -4,36 +4,38 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
-// 🐶 Remplace le type `any` de `doSomeThing` qui est trop généraliste
-//  pour ques les fonctions `doSomeThing` ci-après fonctionnement
-let doSomeThing: any
+  // ✔️ Début de l'exercice
+  // 🐶 Remplace le type `any` de `doSomeThing` qui est trop généraliste
+  //  pour ques les fonctions `doSomeThing` ci-après fonctionnement
+  let doSomeThing: Function
 
-// ❌ NE PAS MODIFIER
-doSomeThing = () => {
-  displayText(`Hello developpeur`)
-}
-doSomeThing()
+  // ❌ NE PAS MODIFIER
+  doSomeThing = () => {
+    displayText(`Hello developpeur`)
+  }
+  doSomeThing()
 
-doSomeThing = (name: string) => {
-  displayText(`Hello ${name}`)
-}
-doSomeThing('John')
-// ❌ END
+  doSomeThing = (name: string) => {
+    displayText(`Hello ${name}`)
+  }
+  doSomeThing("John")
+  // ❌ END
 
-// 🐶 Implemente et spécifie les paramètres et types de la fonction `sum`
-// n'oublie pas de spécifier le type de retour
-function sum() {}
+  // 🐶 Implemente et spécifie les paramètres et types de la fonction `sum`
+  // n'oublie pas de spécifier le type de retour
+  function sum(a: number, b: number): number {
+    return a + b
+  }
 
-// 🚀 N'oublie pas les bonus
+  // 🚀 N'oublie pas les bonus
 
-/*
+  /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
