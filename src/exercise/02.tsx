2a75eec925142e6510e07f4a36f38d199a790d74
Pepito <juniorfotio@teccart.com>
2026-03-16 20:31:18 -0400

    affiche du nombre de personne dans le tableau

--- src/exercise/02.tsx
+++ src/exercise/02.tsx
@@ -4,31 +4,33 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Créer une variable `names` mais sans initiliser de valeurs
-//  ajoute ensuite 'John' et 'doe' à `name` sans utiliser la méthode `push()`
-let names = []
+  // 🐶 Créer une variable `names` mais sans initiliser de valeurs
+  //  ajoute ensuite 'John' et 'doe' à `name` sans utiliser la méthode `push()`
+  let names: string[] = []
 
-// 🤖 affiche 'Il y a 2 noms' dans le navigateur grace à `displayText` et `.length`
+  names = ["John", "doe"]
 
-// 🐶 Créer une constante `games` et initialise avec un tableau vide
-// avec la méthode `push()` ajoute 3 jeux
-// ⛏️ const games
-// 🤖 affiche le nombre d'élément à l'ecran
+  // 🤖 affiche 'Il y a 2 noms' dans le navigateur grace à `displayText` et `.length`
+  displayText(`Il y'a ${names.length} noms`)
+  // 🐶 Créer une constante `games` et initialise avec un tableau vide
+  // avec la méthode `push()` ajoute 3 jeux
+  // ⛏️ const games
+  // 🤖 affiche le nombre d'élément à l'ecran
 
-// 🐶 Créer une constante `things` : un tableau qui peut prendre n'importe quel type de donnée
-// ⛏️ const things
+  // 🐶 Créer une constante `things` : un tableau qui peut prendre n'importe quel type de donnée
+  // ⛏️ const things
 
-// 🤖 affiche le nombre d'élément à l'ecran
+  // 🤖 affiche le nombre d'élément à l'ecran
 
-/*
+  /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
