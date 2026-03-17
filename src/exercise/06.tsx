9dad6704f318130305f898f2b830072741462842
Pepito <juniorfotio@teccart.com>
2026-03-17 03:55:58 -0400

    exercice6 realise avec success

--- src/exercise/06.tsx
+++ src/exercise/06.tsx
@@ -4,36 +4,84 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Déclare un type `Connexion` un tuple contennant toutes les informations utile a la connexion
-// - le nom de la connexion  (ex : Connexion à Google) `string`
-// - le protocole (ex : ftp, http) `string`
-// - le hostname (ex : google.com) `string`
-// - le port  (ex : 423) `number`
-// - le username : (ex : admin) `string`
-// - le password (ex : admin) `string`
-// 📝 documentation tuples https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+  // 🐶 Déclare un type `Connexion` un tuple contennant toutes les informations utile a la connexion
+  type Connexion = [string, string, string, number, string, string]
+  // - le nom de la connexion  (ex : Connexion à Google) `string`
+  // - le protocole (ex : ftp, http) `string`
+  // - le hostname (ex : google.com) `string`
+  // - le port  (ex : 423) `number`
+  // - le username : (ex : admin) `string`
+  // - le password (ex : admin) `string`
+  // 📝 documentation tuples https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
 
-// type Connexion
+  // type Connexion
 
-// 🐶 Créer une variable 'google' qui contiendra les infos de connexion à google
-// https google.com 443
-// Affiche le resultat à l'ecran avec le code
-// 🤖 displayText(`Connexion ${google[0]} : ${google[1]}://${google[2]}:${google[3]} `)
+  // 🐶 Créer une variable 'google' qui contiendra les infos de connexion à google
+  let google: Connexion = [
+    "Google",
+    "http",
+    "google.com",
+    8080,
+    "admin",
+    "admin",
+  ]
+  // https google.com 443
+  // Affiche le resultat à l'ecran avec le code
+  // 🤖 displayText(`Connexion ${google[0]} : ${google[1]}://${google[2]}:${google[3]} `)
+  displayText(
+    `Connexion ${google[0]} : ${google[1]}://${google[2]}:${google[3]} `
+  )
+  // 🐶 créer 3 autres connexions de ton choix et affiche le resultat à l'écran
 
-// 🐶 créer 3 autres connexions de ton choix et affiche le resultat à l'écran
+  let pinterest: Connexion = [
+    "pinterest",
+    "http",
+    "pinterest.com",
+    7015,
+    "guest",
+    "guest",
+  ]
+  let facebook: Connexion = [
+    "facebook",
+    "http",
+    "facebook.com",
+    243,
+    "guest",
+    "guest",
+  ]
+  let youtube: Connexion = [
+    "youtube",
+    "http",
+    "youtube.com",
+    403,
+    "guest",
+    "guest",
+  ]
 
-// 🐶 créer un tableau `connexions` qui contiendra toutes les connexions
-// Ajoute des connexion via l'initialisation et via la méthode `push`
+  displayText(
+    `Connexion ${pinterest[0]} : ${pinterest[1]}://${pinterest[2]}:${pinterest[3]} `
+  )
+  displayText(
+    `Connexion ${facebook[0]} : ${facebook[1]}://${facebook[2]}:${facebook[3]} `
+  )
+  displayText(
+    `Connexion ${youtube[0]} : ${youtube[1]}://${youtube[2]}:${youtube[3]} `
+  )
+  // 🐶 créer un tableau `connexions` qui contiendra toutes les connexions
+  let connexions: Connexion[] = [facebook]
 
-/*
+  // Ajoute des connexion via l'initialisation et via la méthode `push`
+  connexions.push(google, pinterest, youtube)
+  displayText(`il ya ${connexions.length} Connexions`)
+  /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
