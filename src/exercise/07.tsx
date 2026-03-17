b3b613d12b244dcfe274402df694992e560af1a4
Pepito <juniorfotio@teccart.com>
2026-03-17 05:22:24 -0400

    exercice 7 termine

--- src/exercise/07.tsx
+++ src/exercise/07.tsx
@@ -4,58 +4,66 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Modifile le type `car` avec toutes les propriétés demandée par 👨‍✈️ Hugo
-type car = any
-// 🐶 Complete la fonction `kineticEnergy` avec toutes les propriétés demandée par 👨‍✈️ Hugo
-// Fonction prenant en entrée 2 parametre
-// - speed un nombre obligatoire
-// - mass un nombre optionel
-function kineticEnergy() {
-  // calcul l'energie avec cette formule
-  // Energie (en Joule) = 0.5 x masse x vitesse ²
-  // utile Math pour le calcul au carré
-  // 📝 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
-  //
-  // ⛏️ décommente la ligne ci-dessous et met la formule dans result
-  // il s'agit d'une ternaire qui permet de retouner soit 'null' si 'mass' est 'null', soit un 'objet' avec la propriété 'result'
-  //return mass ? {result: _____ } : null
-}
+  // 🐶 Modifile le type `car` avec toutes les propriétés demandée par 👨‍✈️ Hugo
+  type car = {
+    name: string
+    maxSpeed?: number
+    mass?: number
+  }
+  // 🐶 Complete la fonction `kineticEnergy` avec toutes les propriétés demandée par 👨‍✈️ Hugo
+  // Fonction prenant en entrée 2 parametre
 
-let tesla: car
-tesla = {name: 'tesla', mass: 1850, maxSpeed: 78}
+  // - speed un nombre obligatoire
+  // - mass un nombre optionel
+  function kineticEnergy(speed: number, mass?: number) {
+    // calcul l'energie avec cette formule
+    // Energie (en Joule) = 0.5 x masse x vitesse ²
+    // utile Math pour le calcul au carré
+    // 📝 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+    //
+    // ⛏️ décommente la ligne ci-dessous et met la formule dans result
+    // il s'agit d'une ternaire qui permet de retouner soit 'null' si 'mass' est 'null', soit un 'objet' avec la propriété 'result'
+    return mass ? { result: 0.5 * mass * Math.pow(speed, 2) } : null
+  }
 
-let teslaEnergie
-// ⛏️ décommente la ligne ci-dessous et appelle correctement la fonction 'kineticEnergy' avec 'tesla'
-// teslaEnergie = kineticEnergy(.....)
-displayText()
-// ⛏️ décommente sans modifier
-//`energie cinetique de ${tesla.name} est ${teslaEnergie.result} joules`,
+  let tesla: car
+  tesla = { name: "tesla", mass: 1850, maxSpeed: 78 }
 
-// 🐶 le type unknown permet de dire qu'on ne sait pas ce que contiendra la variable
-// nous les utiliseront ici pour pouvoir tester cast de type
-let unknowCarSpeed: unknown = 150
-let unknowCarMass: unknown = 2000
+  let teslaEnergie
+  // ⛏️ décommente la ligne ci-dessous et appelle correctement la fonction 'kineticEnergy' avec 'tesla'
+  teslaEnergie = kineticEnergy(tesla.maxSpeed!, tesla?.mass)
 
-// ⛏️ décommente la ligne ci-dessous et appelle correctement la fonction 'kineticEnergy' avec 'unknowCarSpeed' et 'unknowCarMass'
-// unknowCarEnergie = kineticEnergy(.....)
-let unknowCarEnergie
+  // ⛏️ décommente sans modifier
+  displayText(
+    `energie cinetique de ${tesla.name} est ${teslaEnergie?.result} joules`
+  )
 
-// ⛏️ décommente la ligne ci-dessous
-// displayText(
-//   `energie cinetique est ${unknowCarEnergie.result} joules`
-//   )
+  // 🐶 le type unknown permet de dire qu'on ne sait pas ce que contiendra la variable
+  // nous les utiliseront ici pour pouvoir tester cast de type
+  let unknowCarSpeed: unknown = 150
+  let unknowCarMass: unknown = 2000
 
-/*eslint
+  // ⛏️ décommente la ligne ci-dessous et appelle correctement la fonction 'kineticEnergy' avec 'unknowCarSpeed' et 'unknowCarMass'
+
+  let unknowCarEnergie
+  unknowCarEnergie = kineticEnergy(
+    unknowCarSpeed as number,
+    unknowCarMass as number
+  )!
+  // ⛏️ décommente la ligne ci-dessous
+  displayText(`energie cinetique est ${unknowCarEnergie.result} joules`)
+
+  /*eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-/*eslint
+  /*eslint
   @typescript-eslint/consistent-type-assertions : 'off'
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
