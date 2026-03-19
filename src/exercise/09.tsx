5067a1f6a4d480b89d02ec6bdcd92c02ad9c116b
Pepito <juniorfotio@teccart.com>
2026-03-18 20:55:50 -0400

    exerciccce 9 creation de la classe Dog qui implemente Unnable et Drinkable

--- src/exercise/09.tsx
+++ src/exercise/09.tsx
@@ -4,76 +4,86 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Regarde le code existant et sous le Class 'Cat' créé la classe 'Dog'
-interface IName {
-  name: string
-  printName: () => string
-}
-interface Runnable {
-  run: () => void
-}
-interface Drinkable {
-  drink: () => void
-}
-
-class Animal implements IName {
-  // name: string | undefined
-  nbLeg: number
-  underwater: boolean
-  constructor(name: string, nbLeg: number, underwater: boolean) {
-    this.name = name
-    this.nbLeg = nbLeg
-    this.underwater = underwater
+  // 🐶 Regarde le code existant et sous le Class 'Cat' créé la classe 'Dog'
+  interface IName {
+    name: string
+    printName: () => string
   }
-  name: string
-  printName() {
-    console.log(`Mon nom est ${this.name}`)
-    return this.name
+  interface Runnable {
+    run: () => void
   }
-}
-
-class Cat extends Animal implements Runnable, Drinkable {
-  constructor(name: string) {
-    super(name, 4, false)
+  interface Drinkable {
+    drink: () => void
   }
-  run() {
-    console.log(`Je cours`)
+
+  class Animal implements IName {
+    // name: string | undefined
+    nbLeg: number
+    underwater: boolean
+    constructor(name: string, nbLeg: number, underwater: boolean) {
+      this.name = name
+      this.nbLeg = nbLeg
+      this.underwater = underwater
+    }
+    name: string
+    printName() {
+      console.log(`Mon nom est ${this.name}`)
+      return this.name
+    }
   }
-  drink() {
-    console.log(`Je bois`)
+
+  class Cat extends Animal implements Runnable, Drinkable {
+    constructor(name: string) {
+      super(name, 4, false)
+    }
+    run() {
+      console.log(`Je cours`)
+    }
+    drink() {
+      console.log(`Je bois`)
+    }
   }
-}
 
-// 🐶 Implemente correctement la classe 'Dog'
-// ⛏️ Décommente la classe 'dog' ci-dessous et constate le message d'erreur
-//class Dog extends Animal implements Runnable, Drinkable {
-// 🐶 implétmente correctement dog
-//}
+  // 🐶 Implemente correctement la classe 'Dog'
+  // ⛏️ Décommente la classe 'dog' ci-dessous et constate le message d'erreur
+  class Dog extends Animal implements Runnable, Drinkable {
+    constructor(name: string) {
+      super(name, 4, false)
+    }
 
-// 🐶 Créé une interface 'Swimable' contenant la fonction 'swim()'
+    run() {
+      console.log("je cours")
+    }
 
-// 🐶 adapte la classe Fish en étantdant 'annimal' et implementant 'Swimable'
-class Fish {}
+    drink() {
+      console.log("Je bois")
+    }
+  }
 
-const tigrou = new Cat('Tigrou')
-tigrou.run()
-tigrou.drink()
-displayText(`Nom du chat ${tigrou.printName()}`)
+  // 🐶 Créé une interface 'Swimable' contenant la fonction 'swim()'
 
-// 🐶 Créé une instance de Dog et appelle les fonctions 'run' et 'drink'
-// utilise 'displayText' pour afficher son nom à l'écran
+  // 🐶 adapte la classe Fish en étantdant 'annimal' et implementant 'Swimable'
+  class Fish {}
 
-// 🐶 Créé une instance de Fish et appelle la fonction 'swim'
-// utilise 'displayText' pour afficher son nom à l'écran
+  const tigrou = new Cat("Tigrou")
+  tigrou.run()
+  tigrou.drink()
+  displayText(`Nom du chat ${tigrou.printName()}`)
 
-/*eslint
+  // 🐶 Créé une instance de Dog et appelle les fonctions 'run' et 'drink'
+  // utilise 'displayText' pour afficher son nom à l'écran
+
+  // 🐶 Créé une instance de Fish et appelle la fonction 'swim'
+  // utilise 'displayText' pour afficher son nom à l'écran
+
+  /*eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
